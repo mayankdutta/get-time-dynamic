6cacbf0fe807a1c79b38f75b938b329662c2aabc
Mayank Sharma <info@Mayanks-MacBook-Air.local>
2021-05-08 19:16:41 +0530

    challange complete but doubt remains

--- src/App.js
+++ src/App.js
@@ -1,8 +1,45 @@
 import logo from "./logo.svg";
 import "./App.css";
+import { useState } from "react";
 
 function App() {
-  return <div className="header"></div>;
+  let now =
+    new Date().getHours() +
+    ":" +
+    new Date().getMinutes() +
+    ":" +
+    new Date().getSeconds();
+
+  const [count, setCount] = useState(now);
+
+  function update() {
+    let now =
+      new Date().getHours() +
+      ":" +
+      new Date().getMinutes() +
+      ":" +
+      new Date().getSeconds();
+    setCount(now);
+  }
+
+  /*
+   * Didn't get the flow of react
+   * in which direction it is working
+   * first return function then update then setCount ??
+   */
+
+  setInterval(update, 1000);
+
+  return (
+    <div className="items-center w-screen h-screen header grid justify-items-center">
+      <div
+        className="inline px-4 py-2 m-2 font-sans text-3xl bg-red-300 border-8 border-red-300 rounded-full hover:border-red-600"
+        onClick={update}
+      >
+        {count}
+      </div>
+    </div>
+  );
 }
 
 export default App;
